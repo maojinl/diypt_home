58fc3ae08d98fa840db732222eb10492754fd8cb
Jerry Mao <maojinl@hotmail.com>
2017-11-13 23:03:03 +1100

    error log updated.

--- App_Code/PrizeLogs.cs
+++ App_Code/PrizeLogs.cs
@@ -26,32 +26,35 @@ public static class PrizeLogs
 		}
 	}
 
-    static public void SaveSystemErrorLog(int memberId, int? memberPlanId, PrizeConstants.SystemErrorLevel level,
-        string sWebPage, string sEvent, Exception ex)
-    {
-        using (DIYPTEntities db = new DIYPTEntities())
-        {
-            var log = new PrizeErrorLog();
-            log.MemberExercisePlanId = memberId;
-            log.MemberExercisePlanId = memberPlanId;
-            log.Page = sWebPage; //HttpContext.Current.Request.Url.AbsolutePath;'
-            log.ErrorLevel = (int)level;
-            log.LogDate = PrizeCommonUtils.GetSystemDate();
-            log.Event = sEvent;
+	static public void SaveSystemErrorLog(int memberId, int? memberPlanId, PrizeConstants.SystemErrorLevel level,
+		string sWebPage, string sEvent, Exception ex)
+	{
+		using (DIYPTEntities db = new DIYPTEntities())
+		{
+			var log = new PrizeErrorLog();
+			log.MemberExercisePlanId = memberId;
+			log.MemberExercisePlanId = memberPlanId;
+			log.Page = sWebPage; //HttpContext.Current.Request.Url.AbsolutePath;'
+			log.ErrorLevel = (int)level;
+			log.LogDate = PrizeCommonUtils.GetSystemDate();
+			log.Event = sEvent;
 
-            // Get stack trace for the exception with source file information
-            var st = new StackTrace(ex, true);
-            // Get the top stack frame
-            var frame = st.GetFrame(st.FrameCount - 1);
-            // Get the line number from the stack frame
-            var line = frame.GetFileLineNumber();
-            string sError = ex.Message + " Line_" + frame.GetFileLineNumber();
+			// Get stack trace for the exception with source file information
+			var st = new StackTrace(ex, true);
+			// Get the top stack frame
+			var frame = st.GetFrame(st.FrameCount - 1);
+			// Get the line number from the stack frame
+			var line = frame.GetFileLineNumber();
+			string sError = ex.Message + " Line_" + frame.GetFileLineNumber() + "\r\n" + ex.StackTrace;
 
-            log.Error = sError;
-            log.InnerError = ex.InnerException == null? "" : ex.InnerException.Message;
+			log.Error = sError;
+			log.InnerError = "";
+			if (ex.InnerException != null)
+			{
+				log.InnerError = ex.InnerException.Message + "\r\n" + ex.InnerException.StackTrace;
 
-            db.PrizeErrorLogs.Add(log);
-            db.SaveChanges();
-        }
-    }
+			db.PrizeErrorLogs.Add(log);
+			db.SaveChanges();
+		}
+	}
 }
